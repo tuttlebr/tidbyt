6a1a3c25b6b8dc249a464a93197d097b20c0d6c5
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2023-01-03 03:53:01 +0000

    update the daemonize script
    Signed-off-by: Brandon Tuttle <tuttlebr@ncc-1701.biz>

--- prometheus-monitoring/daemonize-push.sh
+++ prometheus-monitoring/daemonize-push.sh
@@ -2,8 +2,6 @@
 while [ $(curl -o /dev/null -s -w "%{http_code}\n" "http://192.168.1.235:30500/api/v1/query?query=sum(DCGM_FI_DEV_POWER_USAGE)") == 200 ];
     do 
         pixlet render /workspace/prometheus-monitoring.star
-        CURRENT_KWH=$(curl --no-progress-meter "http://192.168.1.235:30500/api/v1/query?query=sum(DCGM_FI_DEV_POWER_USAGE)" | jq '.["data"]["result"][0]["value"][1]')
         pixlet push ${DEVICE_ID} /workspace/prometheus-monitoring.webp
-        echo "Current kWh: ${CURRENT_KWH}"
-        sleep 10
+        sleep 30
 done
